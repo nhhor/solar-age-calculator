06ce1b4792212b78d37de0d0bcc022a420418578
Noah Horwitz <00noah@gmail.com>
2019-11-15 09:32:44 -0800

    add Venus logic and test --passed.

--- src/backend.js
+++ src/backend.js
@@ -3,17 +3,26 @@ export class SolarAge {
     this.earthAge = earthAge;
   }
 
-  // Getter
+  // Mercury Method
+  calcMercury() {
+    return parseFloat((this.earthAge / 0.24).toFixed(2));
+  }
+  // Mercury Getter
   get mercury() {
     return this.calcMercury();
   }
 
-  // Method
-  calcMercury() {
-    return parseFloat((this.earthAge / 0.24).toFixed(2));
+  // Venus Method
+  calcVenus() {
+    return parseFloat((this.earthAge / 0.62).toFixed(2));
   }
+  // Venus Getter
+  get venus() {
+    return this.calcVenus();
+  }
+
 }
 
 let earthAge = new SolarAge(10);
-
-console.log(earthAge.mercury); // 41.67
+console.log("Mercury: ",earthAge.mercury); // 41.67
+console.log("Venus: ",earthAge.venus); // 16.13
